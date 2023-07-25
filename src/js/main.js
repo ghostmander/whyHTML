8007caa2cd47db8eb6671a2692aafc1e3d35b610
Jatin-Jindal <jatinjindal.2003@gmail.com>
2023-07-25 16:49:08 +0530

    last commit

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+
+const x = Math.max($(document).height(), $(window).height());
+console.log(x);
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  const percScroll = y / x;
+  if (y < 0.2) $("#slide-1").fadeIn();
+  else $("#slide-1").fadeOut();
+
+  if (y < 0.4 && y > 0.2) $("#slide-2").fadeIn();
+  else $("#slide-2").fadeOut();
+
+  if (y < 0.6 && y > 0.4) $("#slide-3").fadeIn();
+  else $("#slide-3").fadeOut();
+
+  if (y < 0.8 && y > 0.6) $("#slide-4").fadeIn();
+  else $("#slide-4").fadeOut();
+
+  if (y < 1 && y > 0.8) $("#slide-5").fadeIn();
+  else $("#slide-5").fadeOut();
+});
